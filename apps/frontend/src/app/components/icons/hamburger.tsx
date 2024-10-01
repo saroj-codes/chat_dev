9292754091fefe12e45b7ab3762aea59fffb07f3
saroj-codes <gurungsaroj98k@gmail.com>
2024-10-01 09:35:32 +0545

    frontend setup

---
+++ apps/frontend/src/app/components/icons/hamburger.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Hamburger = () => {
+  return (
+    <svg
+      width="24"
+      height="16"
+      viewBox="0 0 24 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M-0.000244141 2.66764V0.000976562H23.9998V2.66764H-0.000244141ZM-0.000244141 9.33431H23.9998V6.66764H-0.000244141V9.33431ZM-0.000244141 16.001H23.9998V13.3343H-0.000244141V16.001Z"
+        fill="#BBC8FC"
+      />
+    </svg>
+  );
+};
+
+export default Hamburger;
